f1e8b809cb739154fe3ea9fe7120bd073c151d8d
Toknnick <106018075+Toknnick@users.noreply.github.com>
2022-06-24 17:51:48 +0500

    Update Program.cs

--- Task2.3/Program.cs
+++ Task2.3/Program.cs
@@ -7,10 +7,10 @@ namespace Task2._3
         static void Main(string[] args)
         {
             int[] arrayOfNumbers = {5, 12, 19, 26, 33, 40, 47, 54, 61, 68, 75, 82, 89, 96};
-            int numbersInArray = 14;
+            int amountOfNumbersInArray = 14;
 
             //Более удобное условие цикла.
-            for (int i = 0; i < numbersInArray; i++)
+            for (int i = 0; i < amountOfNumbersInArray; i++)
             {
                 Console.WriteLine(arrayOfNumbers[i]);
             }
